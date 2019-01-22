ff6d1f14a3b328578d8a5cf46ba349439ee09418
Robert Hoehndorf <leechuck@leechuck.de>
2019-01-22 21:58:56 +0300

    updated loss 2

--- elembedding.jl
+++ elembedding.jl
@@ -217,7 +217,7 @@ function loss2(x::Tuple{Int64,Int64,Int64})
 end
 
 function loss3(c::Int, d::Int, r::Int) # normal form 3
-    return max(0, euclidean(centerpoint(c) - v(r), centerpoint(d)) + abs(radius(d)) - abs(radius(c)) + args["margin"])
+    return max(0, euclidean(centerpoint(d) - v(r), centerpoint(c)) + abs(radius(d)) - abs(radius(c)) + args["margin"])
 end
 
 function loss3(x::Tuple{Int64,Int64,Int64})
